d14ef7ffb7bae35a7de2881fd34850260baeb082
Raulgaray <85389355+Raulgaray@users.noreply.github.com>
2023-12-12 14:39:55 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Models/ProjectContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Detail> Details { get; set; }
+    }
+}
